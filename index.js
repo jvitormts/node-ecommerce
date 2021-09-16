85da2a71f03d4d34f9a5d2559ad25b899dd82804
Joao Vitor <jvmartins@dev.local>
2021-09-16 08:57:11 -0300

    Conexao com banco - mongoose

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-mongoose.connect("mongodb://jvmartins:Tq6wPm1zdBcmGb6c@cluster0-shard-00-00.ggwx7.mongodb.net:27017,cluster0-shard-00-01.ggwx7.mongodb.net:27017,cluster0-shard-00-02.ggwx7.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mmtv6t-shard-0&authSource=admin&retryWrites=true&w=majority").then(() => console.log("DBConnection Successfull !")).catch((err) => {
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connection Successfull")).catch((err) => {
     console.log(err);
 });
 
 
-
-
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running 1");
 })
