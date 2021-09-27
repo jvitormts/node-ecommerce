df83ac6f44c617637626975ca4eb820d05f78bb7
João Vitor Martins <jvitor.smartins@gmail.com>
2021-09-27 09:26:44 -0300

    Autenticação - Endpoint de registro de usuário

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connection Su
 
 app.use(express.json());
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 
 app.listen(process.env.PORT || 5000, () => {
